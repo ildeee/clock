ada894751e499d85c00761de1f2561aa416075e9
ildeee <112888167+ildeee@users.noreply.github.com>
2022-10-09 11:54:52 +0530

    Update clock.py

--- clock/clock.py
+++ clock/clock.py
@@ -1,19 +1,21 @@
 import time
 from clock_funcs import *
+     
 
-rad = 100
+rad=100
 
 tur.ht()
 tur.tracer(0)
 
-clock(rad, (0, 0))
+
+clock(rad,(0,0))
 
 while True:
-    tym = time.localtime()
-    tymexpand = (tym.tm_hour, tym.tm_min, tym.tm_sec)
-    angles = time_angle(tymexpand)
-    scene(angles, rad)
+    tym=time.localtime()
+    tymexpand=(tym.tm_hour,tym.tm_min,tym.tm_sec)
+    angles=time_angle(tymexpand)
+    scene(angles,rad)
     tur.update()
     time.sleep(1)
-    clearscene(angles, rad)
+    clearscene(angles,rad)
     tur.update()
